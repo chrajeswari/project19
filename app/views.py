601525e70c22d652d8c45f0906d9af4c9281051a
chrajeswari <ch.raji@32003@gmail.com>
2023-12-15 14:30:14 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import *
+
+def display_topic(request):
+    QLTO=Topic.objects.all()
+    d={'topics':QLTO}
+    return render(request,'display_topic.html',d)
+
+
+def display_webpage(request):
+    QLWO=Webpage.objects.all()
+    d={'webpages':QLWO}
+    return render(request,'display_webpage.html',d)
+
+def display_access(request):
+    QLAO=AccessRecord.objects.all()
+    d={'access':QLAO}
+    return render(request,'display_access.html',d)
+
+
+
+
+def insert_topic(request):
+    tn=input('enter topic_name')
+    NTO=Topic.objects.get_or_create(topic_name=tn)[0]
+    NTO.save()
+
+    QLTO=Topic.objects.all()
+    d={'topics':QLTO}
+    return render(request,'display_topic.html',d)
+
+
+
+
+def insert_webpage(request):
+    tn=input('enter topic_name')
+    n=input('enter name')
+    u=input('enter url')
+    to=Topic.objects.get(topic_name=tn)
+    NWO=Webpage.objects.get_or_create(topic_name=to,name=n,url=u)[0]
+    NWO.save()
+
+    QLWO=Webpage.objects.all()
+    d={'webpages':QLWO}
+    return render(request,'display_webpage.html',d)
+
+
+
+
+def insert_access(request):
+    pk=int(input('enter pk value of wabpage'))
+    a=input('enter author')
+    d=input('enter date')
+    WO=Webpage.objects.get(pk=pk)
+    NAO=AccessRecord.objects.get_or_create(name=WO,author=a,date=d)[0]
+    NAO.save()
+
+    QLAO=AccessRecord.objects.all()
+    d={'access':QLAO}
+    return render(request,'display_access.html',d)
+
+
+
+   
--- project20/urls.py
+++ project20/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display_topic',display_topic,name='display_topic'),
+    path('display_webpage',display_webpage,name='display_webpage'),
+    path('display_access',display_access,name='display_access'),
+    path('insert_topic/',insert_topic,name='insert_topic'),
+    path('insert_webpage/',insert_webpage,name='insert_webpage'),
+    path('insert_access/',insert_access,name='insert_access')
 ]
